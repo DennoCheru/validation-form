d14a93f0e341a1275f6f4b2498cd3196527a5a3e
Cheru <dennocheruh@gmail.com>
2025-01-07 14:06:20 +0300

    Validate Correct Field

--- src/dom.js
+++ src/dom.js
@@ -1,18 +1,23 @@
 function addError(field) {
     const errorField = document.createElement('span');
+    errorField.textContent = '';
     errorField.classList.add('error-message');
     field.parentNode.insertBefore(errorField, field.nextSibling);
 }
 
 function showError(field, message) {
-    const errorField = field.nextSibling;
-    errorField.textContent = message;
-    errorField.style.display = 'block';
+    const errorField = field.parentNode.querySelector('.error-message');
+    if (errorField) {
+        errorField.textContent = message;
+        errorField.style.display = 'block';
+    }
 }
 
 function hideError(field) {
-    const errorField = field.nextElementSibling;
-    errorField.style.display = 'none';
+    const errorField = field.parentNode.querySelector('.error-message');
+    if (errorField) {
+        errorField.style.display = 'none';
+    }
 }
 
 export { addError, showError, hideError }
