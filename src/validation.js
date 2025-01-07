0b519abd35763123e46b05b839e12f9974e6a589
Cheru <dennocheruh@gmail.com>
2025-01-07 13:27:49 +0300

    Add validation logic
    Validation logic for the form fields

---
+++ src/validation.js
@@ -0,0 +1,108 @@
+import { showError, hideError } from './dom.js';
+
+class Validator{
+    constructor(form) {
+        this.form = form;
+        this.fields = {
+            email: form.querySelector('#email'),
+            country: form.querySelector('#country'),
+            zip: form.querySelector('#zip-code'),
+            password: form.querySelector('#password'),
+            confirmPassword: form.querySelector('#confirm-pasword'),
+        };
+    }
+
+    validateEmail() {
+        const emailField = this.fields.email;
+        const email = emailField.value.trim();
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!email) {
+            showError(email, 'Email is required');
+            return false;
+        } else if (!emailPattern) {
+            showError(emailField, 'Please enter a valid email address.');
+            return false
+        }
+
+        hideError(emailField);
+        return true;
+    }
+
+    validateCountry() {
+        const countryField = this.fields.country;
+        const country = countryField.value.trim();
+
+        if (!country) {
+            showError(countryField, 'Country is required.')
+            return false;
+        }
+
+        hideError(countryField);
+        return true;
+    }
+
+    validateZip() {
+        const zipCodeField = this.fields.zipCode;
+        const zipCode = zipCodeField.value.trim();
+
+        if(!zipCode) {
+            showError(zipCodeField, 'Zip Code is required');
+            return false;
+        }
+
+        hideError(zipCodeField);
+        return true;
+    }
+
+    validatePassword() {
+        const passwordField = this.fields.password;
+        const password = passwordField.value.trim();
+        
+        if (!password) {
+            showError(passwordField, 'Password is required');
+            return false;
+        } else if (password.length < 8) {
+            showError(passwordField, 'Password must be at least 8 characters long.')
+            return false;
+        }
+
+        hideError(passwordField);
+        return true;
+    }
+
+    validateConfirmPassword() {
+        const passwordField = this.fields.password;
+        const confirmPasswordField = this.fields.confirmPassword;
+        const confirmPassword = confirmPasswordField.value.trim();
+
+        if (!confirmPassword) {
+            showError(confirmPasswordField, 'Please confirm your password');
+            return false;
+        } else if (confirmPassword !== passwordField.value.trim()) {
+            showError(confirmPasswordField, 'Passwords do not match');
+            return false;
+        }
+
+        hideError(confirmPasswordField)
+        return true;
+    }
+
+    validateAll() {
+        const isEmailValid = this.validateEmail();
+        const isCountryValid = this.validateCountry();
+        const isZipCodeValid = this.validateZip();
+        const isPasswordValid = this.validatePassword();
+        const isConfirmPasswordValid = this.validateConfirmPassword();
+
+        return (
+            isEmailValid &&
+            isCountryValid &&
+            isZipCodeValid &&
+            isPasswordValid &&
+            isConfirmPasswordValid
+        );
+    }
+}
+
+export { Validator }
